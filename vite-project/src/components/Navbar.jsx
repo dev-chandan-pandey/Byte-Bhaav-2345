78ad3f8e257ef743fc37d8c4a13ef5c4d323e56d
khadimkhan5674 <khadimkhan5674@gmail.com>
2024-03-29 14:25:44 +0530

    Changes in navigation section

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -1,14 +1,7 @@
 import React from 'react'
 
 function Navbar() {
-    const navItems = (<>
-    <li className="text-lg"><a>Home</a></li>
-      <li className="text-lg"><a>Menu</a></li>
-      <li className="text-lg"><a>Shop</a></li>
-      <li className="text-lg"><a>Blog</a></li>
-      <li className="text-lg"><a>About</a></li>
-      <li className="text-lg"><a>Contact</a></li>
-    </>)
+  
   return (
     <>
      <div className='max-w-screen-2xl container max-auto md:px-20 px-4'>
@@ -19,7 +12,12 @@ function Navbar() {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-      {navItems}
+      <li className="text-lg"><a>Home</a></li>
+      <li className="text-lg"><a>Menu</a></li>
+      <li className="text-lg"><a>Shop</a></li>
+      <li className="text-lg"><a>Blog</a></li>
+      <li className="text-lg"><a>About</a></li>
+      <li className="text-lg"><a>Contact</a></li>
       </ul>
     </div>
     <a className=" text-2xl font-bold cursor-pointer">DelightDish</a>
@@ -27,7 +25,12 @@ function Navbar() {
  
   <div className="navbar-center hidden lg:flex ">
     <ul className="menu menu-horizontal px-1 space-x-6">
-     {navItems}
+    <li className="text-lg"><a>Home</a></li>
+      <li className="text-lg"><a>Menu</a></li>
+      <li className="text-lg"><a>Shop</a></li>
+      <li className="text-lg"><a>Blog</a></li>
+      <li className="text-lg"><a>About</a></li>
+      <li className="text-lg"><a>Contact</a></li>
     </ul>
   </div>
   <div className='navbar-end space-x-3'>
