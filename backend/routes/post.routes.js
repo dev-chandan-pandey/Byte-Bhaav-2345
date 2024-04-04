9a9bac1dba05562f630e8a528b300f29574ca51b
VishnurajKarockal <vishnurajkarockal@gmail.com>
2024-04-04 19:43:45 +0530

    Updated backend cloudinary credentials

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -4,15 +4,15 @@ const { postModel } = require("../model/post.model")
 const { auth } = require("../middleware/auth.middleware")
 const { upload } = require("../middleware/multer.middleware")
 const cloudinary = require("cloudinary").v2
-
+require("dotenv").config()
 const postRouter = express.Router()
 postRouter.use(express.json())
 
 // add new post
 cloudinary.config({
-	cloud_name: "ddoxnokr6",
-	api_key: "675629927262363",
-	api_secret: "uJRnxhxld0jLqPnjOcUd9WeYvUQ",
+	cloud_name: process.env.cloudinary_cloud_name,
+	api_key: process.env.cloudinary_api_key,
+	api_secret: process.env.cloudinary_api_secret,
 })
 postRouter.post("/register", auth, upload.single("image"), async (req, res) => {
 	console.log("Vishnuraj kr")
